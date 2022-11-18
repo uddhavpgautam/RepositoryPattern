29f66508861bae6aca848cb7374157680b3d5af9
uddhavpgautam <infofuniversities@gmail.com>
2022-11-18 17:06:53 -0500

    viewpagers hosting fragment added

--- app/src/main/java/com/example/repositorypattern/activities/MainActivity.kt
+++ app/src/main/java/com/example/repositorypattern/activities/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.repositorypattern.activities
 
-import CardViewAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 import com.example.newsapp.R
-import com.example.repositorypattern.adapters.ScreenSlidePagerAdapter
-import com.example.repositorypattern.animators.ViewPager2ViewHeightAnimator
+import com.example.repositorypattern.fragments.ViewPager2HostingFragment
 import com.example.repositorypattern.toolbars.utils.CollapsingToolbar
-import com.example.repositorypattern.transformers.ZoomOutPageTransformer
 
 //eval `ssh-agent -s`; ssh-add /Users/roshanidahal/.ssh/id_rsa; git push
 
@@ -16,29 +12,15 @@ import com.example.repositorypattern.transformers.ZoomOutPageTransformer
 //when you have to add other logic and callbacks and you get even hard when you want to migrate
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var viewPagerForFragments: ViewPager2
-    private lateinit var viewPagerForViews: ViewPager2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
-        viewPagerForFragments = findViewById(R.id.pager_for_fragments)
-        viewPagerForFragments.setPageTransformer(ZoomOutPageTransformer())
-        //all adapter used for viewpager2 are RecyclerView.Adapter
-        val screenSlidePagerAdapter = ScreenSlidePagerAdapter(this)
-        viewPagerForFragments.adapter = screenSlidePagerAdapter
-
-        viewPagerForViews = findViewById(R.id.pager_for_views)
-        viewPagerForViews.setPageTransformer(ZoomOutPageTransformer())
-        val cardViewAdapter = CardViewAdapter()
-        viewPagerForViews.adapter = cardViewAdapter
-
-        val viewPager2ViewHeightAnimator = ViewPager2ViewHeightAnimator()
-        viewPager2ViewHeightAnimator.viewPager2 = viewPagerForFragments
 
         val collapsingToolbar = CollapsingToolbar(this)
         collapsingToolbar.setCollapsingToolbar()
+
+        //fragment transaction
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.viewpager2_hosting_fragment, ViewPager2HostingFragment.newInstance()).commitNow()
     }
 
 }
---
+++ app/src/main/java/com/example/repositorypattern/fragments/ViewPager2HostingFragment.kt
@@ -0,0 +1,48 @@
+package com.example.repositorypattern.fragments
+
+import CardViewAdapter
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.newsapp.R
+import com.example.repositorypattern.adapters.ScreenSlidePagerAdapter
+import com.example.repositorypattern.animators.ViewPager2ViewHeightAnimator
+import com.example.repositorypattern.transformers.ZoomOutPageTransformer
+
+class ViewPager2HostingFragment : Fragment() {
+
+    private lateinit var viewPagerForFragments: ViewPager2
+    private lateinit var viewPagerForViews: ViewPager2
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_view_pager2_hosting, container, false)
+
+
+        viewPagerForFragments = view.findViewById(R.id.pager_for_fragments)
+        viewPagerForFragments.setPageTransformer(ZoomOutPageTransformer())
+        //all adapter used for viewpager2 are RecyclerView.Adapter
+        val screenSlidePagerAdapter = ScreenSlidePagerAdapter(requireActivity())
+        viewPagerForFragments.adapter = screenSlidePagerAdapter
+
+        viewPagerForViews = view.findViewById(R.id.pager_for_views)
+        viewPagerForViews.setPageTransformer(ZoomOutPageTransformer())
+        val cardViewAdapter = CardViewAdapter()
+        viewPagerForViews.adapter = cardViewAdapter
+
+        val viewPager2ViewHeightAnimator = ViewPager2ViewHeightAnimator()
+        viewPager2ViewHeightAnimator.viewPager2 = viewPagerForFragments
+
+        return view
+    }
+
+    companion object {
+        fun newInstance() = ViewPager2HostingFragment()
+    }
+
+}
